1f7b1f542537ea09c630001599ca34c464051a92
fpunny <frederic.pun@gmail.com>
2021-02-25 05:59:01 -0500

    Added buttons, extended colors, fixed mixin.transition

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { ClassNamesPropType } from '../../utils/proptypes';
+import Text from '../Text';
+import styles from './Button.module.scss';
+
+export default function Button({
+  rightIcon: RightIcon,
+  leftIcon: LeftIcon,
+  color = `lime`,
+  type = `solid`,
+  as = `button`,
+  className,
+  children,
+  ...props
+}) {
+  return (
+    <Text
+      {...props}
+      className={classNames(
+        styles[`button--color-${color}`],
+        styles[`button--type-${type}`],
+        styles.button,
+        className,
+      )}
+      lineHeight='normal'
+      weight='bold'
+      type='body1'
+      as={as}
+    >
+      {LeftIcon && (
+        <span className={classNames(styles[`icon--left`], styles.icon)}>
+          <LeftIcon />
+        </span>
+      )}
+      <span>{children}</span>
+      {RightIcon && (
+        <span className={classNames(styles[`icon--right`], styles.icon)}>
+          <RightIcon />
+        </span>
+      )}
+    </Text>
+  );
+}
+
+Button.propTypes = {
+  as: PropTypes.elementType,
+  className: ClassNamesPropType,
+  rightIcon: PropTypes.elementType,
+  leftIcon: PropTypes.elementType,
+  color: PropTypes.string,
+  type: PropTypes.oneOf([`solid`, `outline`, `ghost`]),
+  children: PropTypes.node,
+};
