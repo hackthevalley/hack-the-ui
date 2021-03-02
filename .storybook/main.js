a72a1273c08a59e472f542dcbfe061a0a11fd7f3
fpunny <frederic.pun@gmail.com>
2021-03-01 22:05:22 -0500

    Added raw svg + more fonts

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,21 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx'],
+  stories: [`../docs/**/*.stories.mdx`, `../src/**/*.stories.mdx`],
   babel: async (options) => ({
     ...options,
     plugins: [...options.plugins, `babel-plugin-dev-expression`],
   }),
   addons: [
     {
-      name: '@storybook/preset-scss',
+      name: `@storybook/preset-scss`,
       options: {
         cssLoaderOptions: {
           modules: {
-            localIdentName: '[name]__[local]--[hash:base64:5]',
+            localIdentName: `[name]__[local]--[hash:base64:5]`,
           },
         },
       },
     },
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    `@storybook/addon-links`,
+    `@storybook/addon-essentials`,
   ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
-import './index.css';
+if (process.env.NODE_ENV === `production`) {
+  require('./index.css');
+} else {
+  require('./index.scss');
+}
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
