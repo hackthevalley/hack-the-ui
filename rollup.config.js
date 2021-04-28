ebcff9b3241c2abc1f13eeace3bc24de62a8621c
Frederic Pun <fredericpun@MBPl19C02F70P9MD6M.local>
2021-04-27 21:01:50 -0400

    owo

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,16 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: pkg.main,
+      file: './dist/index.cjs.js',
       format: 'cjs',
     },
     {
-      file: pkg.module,
+      file: `./dist/index.esm.js`,
       format: 'es',
     },
   ],
