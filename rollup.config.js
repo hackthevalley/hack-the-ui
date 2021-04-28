a0e1ecbb8558be8ec023bbb56f0d9cd12d42e4fc
Frederic Pun <fredericpun@MBPl19C02F70P9MD6M.local>
2021-04-27 20:52:41 -0400

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: ['react'],
+  plugins: [
+    peerDepsExternal(),
+    postcss({
+      extract: false,
+      modules: true,
+      use: ['sass'],
+    }),
+    babel({ exclude: 'node_modules/**' }),
+    resolve({
+      extensions: ['.js', '.jsx'],
+    }),
+    commonjs(),
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './components/Button';
+export { default as Card } from './components/Card';
+export { default as Section } from './components/Section';
+export { default as Text } from './components/Text';
